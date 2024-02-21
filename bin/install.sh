9e50c699237ab615fbc2afdbce4289d6c8f076d3
Shibuya Keita <shibuya.keita@cloud-ace.jp>
2024-02-21 15:15:54 +0900

    fix: CI の構成を修正

--- bin/install.sh
+++ bin/install.sh
@@ -1,16 +1,17 @@
 #!/bin/zsh
 
-DOTFILES_DIR=~/github.com/KeitaSHIBUYA/dotfiles
+# dotfiles/ で実行すること
+DOTFILES_DIR=./
 
 echo "Starting to create symbolic links for dotfiles..."
 
 # "."から始まるファイルをシンボリックリンク
 for file in $(find $DOTFILES_DIR -name ".*" -type f); do
-  ln -snf $file ~/
+  ln -snf $file ~/ || exit 1
 done
 
 # iceberg.vimをシンボリックリンク
-mkdir -p ~/.vim/colors
-ln -snf $DOTFILES_DIR/.vim/colors/iceberg.vim ~/.vim/colors/
+mkdir -p ~/.vim/colors || exit 1
+ln -snf $DOTFILES_DIR/.vim/colors/iceberg.vim ~/.vim/colors/ || exit 1
 
 echo "Successfully created symbolic links for dotfiles."
