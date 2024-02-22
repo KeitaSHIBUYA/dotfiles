c2777e45e4150f119fbfc6ed0908e00628f523d5
Shibuya Keita <shibuya.keita@cloud-ace.jp>
2024-02-22 15:52:44 +0900

    delete: #7 不要なファイルを削除

--- bin/brew_install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# Check if Homebrew is already installed
-if ! command -v brew &> /dev/null; then
-    echo "Homebrew is not installed. Installing..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-    echo "Homebrew is already installed."
-fi
